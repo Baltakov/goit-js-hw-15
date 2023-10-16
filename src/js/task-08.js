adffebca645fb907d2491150dad6aa6729dd2d82
Baltakov <tira2500@gmail.com>
2023-10-16 07:30:35 +0300

    Task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Забороняє перезавантаження сторінки під час відправлення форми
+
+  const formData = new FormData(loginForm);
+
+  const userData = {};
+  formData.forEach((value, key) => {
+    userData[key] = value;
+  });
+
+  if (userData.email.trim() === "" || userData.password.trim() === "") {
+    alert("Всі поля повинні бути заповнені");
+  } else {
+    console.log(userData); // Виведення об'єкта в консоль
+    loginForm.reset(); // Очищення полів форми
+  }
+});
