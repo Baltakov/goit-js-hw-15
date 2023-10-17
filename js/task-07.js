c9a1f2863a27200e8f85cf2c1a69765f9ef6734b
Baltakov <tira2500@gmail.com>
2023-10-17 16:21:24 +0300

    Task 7 no jumps

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,15 @@
+// const fontSizeControl = document.querySelector("#font-size-control");
+// const text = document.querySelector("#text");
+
+// // Устанавливаем начальный фонт-сайз спана
+// text.style.fontSize = `${fontSizeControl.value}px`;
+
 const fontSizeControl = document.getElementById("font-size-control");
-const textElement = document.getElementById("text");
+const textSpan = document.getElementById("text");
 
 fontSizeControl.addEventListener("input", () => {
-  const fontSize = fontSizeControl.value;
-  textElement.style.fontSize = fontSize + "px";
+  const fontSize = fontSizeControl.value + "px";
+  textSpan.style.fontSize = fontSize;
 });
+
+textSpan.style.fontSize = fontSizeControl.value + "px";
